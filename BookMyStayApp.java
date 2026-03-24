c486f5df81c95664a6039b2dc15c9da3d003bbc9
Maharnish13 <km6909@srmist.edu.in>
2026-03-24 08:43:26 +0530

    maharnish added UC2

--- BookMyStayApp.java
+++ BookMyStayApp.java
@@ -1,18 +1,86 @@
+
+abstract class Room {
+    protected int beds;
+    protected double price;
+    protected String type;
+
+    // Constructor
+    public Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    // Abstract method
+    public abstract void displayDetails();
+}
+
+// Single Room Class
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 1000);
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println(type + " | Beds: " + beds + " | Price: ₹" + price);
+    }
+}
+
+// Double Room Class
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 2000);
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println(type + " | Beds: " + beds + " | Price: ₹" + price);
+    }
+}
+
+// Suite Room Class
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 5000);
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println(type + " | Beds: " + beds + " | Price: ₹" + price);
+    }
+}
+
 public class BookMyStayApp {
 
-    /**
-     * Main method - Entry point of the application
-     * @param args command line arguments
-     */
     public static void main(String[] args) {
-        
-        // Display welcome message
-        System.out.println("==================================");
-        System.out.println("   Welcome to Book My Stay App   ");
-        System.out.println("   Hotel Booking System v1.0     ");
-        System.out.println("==================================");
-        
-        // Application ends
-        System.out.println("Application started successfully!");
+
+        System.out.println("===== Welcome to Book My Stay App =====");
+
+        // Create Room Objects (Polymorphism)
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        // Display details
+        System.out.println("\nRoom Details & Availability:\n");
+
+        single.displayDetails();
+        System.out.println("Available: " + singleAvailable + "\n");
+
+        doubleRoom.displayDetails();
+        System.out.println("Available: " + doubleAvailable + "\n");
+
+        suite.displayDetails();
+        System.out.println("Available: " + suiteAvailable + "\n");
+
+        System.out.println("=======================================");
     }
 }
