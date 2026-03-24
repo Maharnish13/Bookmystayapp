821674aa8e06f74d6c218ba1bfeb8ea5527136be
Maharnish13 <km6909@srmist.edu.in>
2026-03-24 08:37:14 +0530

    maharnish added UC1

---
+++ BookMyStayApp.java
@@ -0,0 +1,18 @@
+public class BookMyStayApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Display welcome message
+        System.out.println("==================================");
+        System.out.println("   Welcome to Book My Stay App   ");
+        System.out.println("   Hotel Booking System v1.0     ");
+        System.out.println("==================================");
+        
+        // Application ends
+        System.out.println("Application started successfully!");
+    }
+}
